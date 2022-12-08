39127c6deb0dd0aaaf4093f15976eb6298c0a292
radiabekhtaoui <radia.bekhtaoui@univ-tlemcen.dz>
2022-12-08 15:48:58 +0100

    solve system [C]

--- code/codeC/code.c
+++ code/codeC/code.c
@@ -39,9 +39,32 @@ void printMat(double **a,int n) {
     printf("\n");
   }
 }
+void printVect(double *x,int n){
+    for(int i=0;i<n;i++) {
+        printf("%lf \t",x[i]);
+    }
+    printf("\n");
+}
+double  *resoudres(double **l,double **u,double *b,int n) {
+    double *y,*x;
+    
+    x=(double*)malloc(sizeof(double)*n);
+    y=(double*)malloc(sizeof(double)*n);
+
+    y[0]=b[0];
+    for(int i=1;i<n;i++) {
+        y[i]=b[i]-l[i][i-1]*y[i-1];
+    }
+    x[n-1]=y[n-1]/u[n-1][n-1];
+    for(int i=n-2;i>=0;i--) {
+        x[i]=y[i]-u[i][i+1]*x[i+1]/u[i][i];
+    }
+
+    return x;
+}
 
 int main(){
-    double **a, **l, **u;
+    double **a, **l, **u, *b ,*x;
     int i;
 
     a = (double**)malloc(N*sizeof(double*));
@@ -58,7 +81,7 @@ int main(){
         for(i=0;i<N;i++) {
             u[i] = (double*)malloc(N*sizeof(double));
         }
-
+    b= (double*)malloc(sizeof(double)*N);
 
   for(i=1;i<N;i++) {
     a[i-1][i]=-1;
@@ -66,6 +89,11 @@ int main(){
     a[i-1][i-1]=2;
     a[N-1][N-1]=2;
   }
+
+  for (i=0;i<N;i++){
+    b[i]=1;
+  }
+
   printf("Matrice A :\n");
   printMat(a,N);
   decompLU(a,l,u,N);
@@ -73,4 +101,9 @@ int main(){
   printMat(l,N);
   printf("Matrice U :\n");
   printMat(u,N);
+  x=resoudres(l,u,b,N);
+  printf("solution x:\n");
+  printVect(x,N);
+
+  return 0;
 }
