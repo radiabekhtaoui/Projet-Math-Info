df9424d4b26bfe4da62495420d5237d8329c959e
radiabekhtaoui <radia.bekhtaoui@univ-tlemcen.dz>
2022-12-14 17:46:46 +0100

    modifier arborescence et code.c

--- code/codeC/code.c
+++ code/codeC/code.c
@@ -5,10 +5,31 @@
 #include "include/multiplication.h"
 #include "include/test.h"
 
-#define N 3
+// parametres de discretisation
+#define N  5   // nombre de points de discretisation
+#define H  1.0/(N+1)   // le pas d'espace uniforme h = 1/(n+1)
+// conditions de Dirichlet
+#define ALPHA 0  //condition limite u(O)
+#define BETA 0  // condition limite u(1)
+// fonction à resoudre ici j'ai choisit s(x)= 4π²sin(2πx)
+double s(double x) {
+    return 4 * M_PI * M_PI * sin(2 * M_PI * x);
+}
+//fonction maillage 
+double *maillage(int n)
+{ 
+    // Créer un tableau pour stocker les points du maillage
+    double *x = malloc(n+2 * sizeof(double));
 
+    for (int i = 0; i <= n+1; i++)
+    {
+        x[i] =i * H;
+    }
+
+    return x;
+}
 int main(){
-    double **a, **l, **u, *b ,*x;
+    double **a, **l, **u, *b ,*m,*x;
     int i;
 
     a = (double**)malloc(N*sizeof(double*));
@@ -22,10 +43,10 @@ int main(){
     b= (double*)malloc(sizeof(double)*N);
   //definir A 
   for(i=1;i<N;i++) {
-    a[i-1][i]=-1;
-    a[i][i-1]=-1;
-    a[i-1][i-1]=2;
-    a[N-1][N-1]=2;
+    a[i-1][i]=-1/(H*H);
+    a[i][i-1]=-1/(H*H);
+    a[i-1][i-1]=2/(H*H);
+    a[N-1][N-1]=2/(H*H);
   }
   
   printf("Matrice A :\n");
@@ -48,13 +69,21 @@ int main(){
   printf("Matrice U :\n");
   printMat(u,N);
   
-  //definir b:
+  /*calcul des valeurs de b sur chaque point de discretisation
+  m = maillage(N);
+  definir b:
+  b[0]= s(x[1])+(ALPHA/(H*H));
+  b[N-1]= s(x[N-1])+(BETA/(H*H));
+  for (int i = 1; i < N-1; i++){
+      b[i]=s(x[i]);
+  }
+  
+  printf("b:\n");
+  printVect(b,N);*/
+  
   for (i=0;i<N;i++){
     b[i]=1;
   }
-  printf("b:\n");
-  printVect(b,N);
-  
   //resoudre le systeme:
   x=resoudres(l,u,b,N);
   
